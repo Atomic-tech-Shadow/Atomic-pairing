1719914be0db07b300329ad8beb1d5f2b3671979
𒅒𝗔𝗧𝗢𝗠𝗜𝗖ᵗᵉᶜʰ✰ཽ <akuebenjamin01@gmail.com>
2025-08-01 12:04:17 +0000

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "malvin~" + string_session;
+                        let md = "atomic~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*YO 🫩, ATOMIC-XD User!* 👋🏻
 
